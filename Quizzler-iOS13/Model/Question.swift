333828b53375b45db0e944212629be8f32146950
aghaf13@yahoo.com <aghaf13@yahoo.com>
2020-01-07 18:19:35 +0500

    Add quizler

---
+++ Quizzler-iOS13/Model/Question.swift
@@ -0,0 +1,17 @@
+//
+//  Questions.swift
+//  Quizzler-iOS13
+//
+//  Created by Agha Shahriyar Khan on 07/01/2020.
+//  Copyright © 2020 The App Brewery. All rights reserved.
+//
+
+struct Question{
+    var text : String
+    var answer : String
+    
+    init(q:String,a:String) {
+        self.text = q
+        self.answer = a
+    }
+}
---
+++ Quizzler-iOS13/Model/QuizBrain.swift
@@ -0,0 +1,13 @@
+//
+//  QuizBrain.swift
+//  Quizzler-iOS13
+//
+//  Created by Agha Shahriyar Khan on 07/01/2020.
+//  Copyright © 2020 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct QuizBrain {
+    
+}
